ad22014b6bbb3a6956313c4981ca5cf69747cac0
_rahul_yadav_ <rahulbky2004@gmail.com>
2024-06-12 09:50:33 +0000

    Async nature of JS , API requests in JS

---
+++ AsyncJS/asyncjs.js
@@ -0,0 +1,13 @@
+// JavaScript is an asynchronous language but its defualt behaviour is synchronous . We have to forcefully tell it to be asynchronous 
+
+//JavaScript is synchronous and single threaded by default: every line of code 
+// is executed one after the other, and each task must wait for the previous one to be completed before moving to the next
+
+
+// Blocking code                                                         Non Blocking Code
+//Blocks the flow of program                                            //Does not blocks the execution of code , other processes keep on executing until one completes and comesback
+//eg: Read a file Synchronously                                         //Read file Asynchronously
+
+
+//JS engine is made up of Memory Heap and Call Stack
+//JS engine being single threaded is supported by web api and if environment given then its node 
